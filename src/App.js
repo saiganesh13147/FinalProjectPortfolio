53ca10d4739e4000ccfb9895a1f65c828e3404c1
sai ganesh <ganeshsai287@gmail.com>
2021-04-28 20:36:03 -0400

    Added Skills Sections

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Particles from 'react-particles-js'
 import About from './components/About'
+import Skills from './components/Skills'
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
       <Navbar></Navbar>
       <Header></Header>
       <About></About>
+      <Skills />
     </>
   )
 }
